c275ec3c38a6acc84870e45898056f590cca60c2
Valentin Uzhakov <yuz-valya@yandex.ru>
2018-07-23 17:57:58 +0300

    Player add with VkBot

---
+++ Models/ModelViews/PlayerModelView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.ModelViews
+{
+    public class PlayerModelView
+    {
+        public string Username { get; set; }
+        public string VKId { get; set; }
+        public GameType GameName { get; set; }
+    }
+}
---
+++ WolfBack/Services/QueueService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WolfBack.Services.Interfaces;
+using WolfBack.Extencions;
+
+namespace WolfBack.Services
+{
+    public class QueueService : IQueueService
+    {
+        public ConcurrentQueue<Guid> queue = new ConcurrentQueue<Guid>();
+
+        public void PutInQueue(Guid playerId)
+        {
+            queue.Enqueue(playerId);
+        }
+        public List<Guid> GetFromQueue(int count)
+        {
+            return queue.Take(count);
+        }
+    }
+}
