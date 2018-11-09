cff98c02c7e42b775f4f2cd89d5d08724aa9bc18
Uzhakov Valentine <yuz-valya@yandex.ru>
2018-11-09 16:27:26 +0300

    lalka

--- WolfBack/Startup.cs
+++ WolfBack/Startup.cs
@@ -78,7 +78,7 @@ namespace WolfBack
             services.AddSpaStaticFiles(config => config.RootPath = "wwwroot");
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline..
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
